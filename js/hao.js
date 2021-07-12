369e128d006fd19f64133c9c18b08f229769a6df
zhanghao123 <13930333639@163.com>
2021-07-12 15:51:35 +0800

    保存

--- js/hao.js
+++ js/hao.js
@@ -411,6 +411,23 @@ function closeLoading(element = false, loadingTime = false) {
     };
 }
 
+// 求两个值和的精确值
+function accAdd(arg1, arg2){
+    let r1, r2, m;
+    try {
+        r1 = arg1.toString().split(".")[1].length
+    } catch (e) {
+        r1 = 0
+    }
+    try {
+        r2 = arg2.toString().split(".")[1].length
+    } catch (e) {
+        r2 = 0
+    }
+    m = Math.pow(10, Math.max(r1, r2))
+    return (arg1 * m + arg2 * m ) / m
+}
+
 // 获取单个元素
 function queryE (element) {
 	return document.querySelector(element);
