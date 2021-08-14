97dc2478e3f24488abec80bfc959108d25a86846
张浩 <13930333639@163.com>
2021-08-14 22:32:45 +0800

    保存

--- demo.js
+++ demo.js
@@ -1,47 +1,27 @@
-function generateHandle (value1, value2, fn) {
-    let num = value1 + value2
-    return function (vale3, value4) {
-        let obj = {
-            num: num,
-            name: vale3,
-            age: value4
-        }
-        fn(obj)
-    }
-}
-
 function spa (value) {
-    this.originDom = value.originDom ? document.querySelector(value.originDom) : document.body;
+	this.originDom = value.originDom ? document.querySelector(value.originDom) : document.body;
     this.data = value.data || {};
     this.handlers = value.domBindingHandle || {};
     this.monitorHandle = value.monitorHandle || {};
     this.template = value.template || {};
+    this.proxy();
+};
 
-    this.createdDom(this.template, this.originDom).then(() => {
+// 监听template改变事件,更新template必须用这个方法
+spa.prototype.proxy = function(call = () => {}) {
+	call();
+	let fragment = document.createDocumentFragment();
+    this.createdDom(this.template, fragment).then(() => {
+        this.originDom.innerHTML = '';
+    	this.originDom.appendChild(fragment);
         this.setHandle(this.handlers, 'handlers');
         this.setHandle(this.monitorHandle, 'monitorHandle');
     });
-
-    let tem = new Proxy(this.template, {
-        get(target, prop) {
-            return target[prop]
-        },
-        set (target, prop, value) {
-            console.log(value)
-            target[prop] = value
-        }
-    })
 };
 
-// 监听template改变事件
-spa.prototype.proxy = function(data) {
-
-};
-
-// 初始化本页数据
+// 更新本页数据
 spa.prototype.storageSetHandle = function(data) {
-    let storage = this.data,
-        thisArr = Object.keys(storage),
+    let thisArr = Object.keys(this.data),
         ary = Object.keys(data),
         i = 0,
         j = 0,
@@ -50,7 +30,7 @@ spa.prototype.storageSetHandle = function(data) {
     for (i; i < len; i++) {
         // 检查this中是否有要更新的键
         if (thisArr.includes(ary[i])) {
-            storage[ary[i]] = data[ary[i]];
+            this.data[ary[i]] = data[ary[i]];
         };
     };
 };
@@ -58,30 +38,50 @@ spa.prototype.storageSetHandle = function(data) {
 // 初始化dom
 spa.prototype.createdDom = function (template, parent) {
     return new Promise((resolve, reject) => {
-        for (let item in template) {
-            let element = creatE(item);
+    	let parentNameList = Object.keys(template),
+    		i = 0;
+        if (!parentNameList.length) { return };
+        for (i; i < parentNameList.length; i++) {
+        	let name = parentNameList[i];
+            let element = creatE(template[name].nodeName);
 
-
-            let attr = Object.keys(template[item]);
+            let attr = Object.keys(template[name]);
             if (!attr.length) { return };
 
-            for (let edit in template[item]) {
+            for (let edit in template[name]) {
+            	element.setAttribute('id', name)
                 if (edit !== 'template' && edit !== 'innerText') {
-                    element.setAttribute(edit, template[item][edit])
+                    element.setAttribute(edit, template[name][edit])
                 } else if (edit === 'innerText') {
-                    element.innerText = template[item][edit]
+                    element.innerText = template[name][edit]
                 }
             }
-
-            if (template[item].template) {
-                this.createdDom(template[item].template, element);
+            if (template[name].template) {
+                this.createdDom(template[name].template, element);
             }
             parent.appendChild(element);
         }
         resolve();
     })
 };
-
+// 替换节点
+spa.prototype.replace = function (id, template) {
+    let newNode = creatE(template.nodeName),
+    	usedNode = document.querySelector(id);
+    let parent = usedNode.parentNode;
+    let attr = Object.keys(template);
+    if (!attr.length) { return };
+    for (let edit in template) {
+        if (edit !== 'template' && edit !== 'innerText') {
+            newNode.setAttribute(edit, template[edit])
+        } else if (edit === 'innerText') {
+            newNode.innerText = template[edit]
+        } else if (edit === 'template') {
+        	this.createdDom(template[edit], newNode);
+        }
+    }
+	parent.replaceChild(newNode, usedNode)
+};
 // 初始化方法
 spa.prototype.setHandle = function (list, type) {
     let handleList = Object.keys(list), i = handleList.length - 1;
@@ -110,19 +110,31 @@ spa.prototype.addHandler = function (element, type, handler) {
         dom.attachEvent('on' + type, handler);
     } else {
         // 这里使用DOM0级
-        dom['on' + type] = null;
+        dom['on' + type] = handler;
     }
 };
 // 清理事件处理程序
-spa.prototype.removeHandler = function (element, type, handler) {
+spa.prototype.removeHandler = function (dom, type, handler) {
+	let element = document.querySelector(dom);
     if (element.addEventListener) {
         // 判断是否支持DOM二级
-        element.addEventListener(type, handler, false);
+        element.removeEventListener(type, handler);
     } else if (element.attachEvent) {
         // 判断是否是IE
-        element.attachEvent('on' + type, handler);
+        element.detachEvent('on' + type, handler);
     } else {
         // 这里使用DOM0级
         element['on' + type] = null;
+    }
+}
+function generateHandle (value1, value2, fn) {
+    let num = value1 + value2
+    return function (vale3, value4) {
+        let obj = {
+            num: num,
+            name: vale3,
+            age: value4
+        }
+        fn(obj)
     }
 }
